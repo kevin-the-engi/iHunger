7331452657679e9e146864d30f012dca295a079b
Kevin Ng <kevin.the.engi@gmail.com>
2021-03-29 20:48:49 -0700

    finished setup

---
+++ client/src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        App
+      </div>
+    )
+  }
+};
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../client/dist')))
+
+app.get('/test', function (req, res) {
+  res.send('hello world')
+})
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
